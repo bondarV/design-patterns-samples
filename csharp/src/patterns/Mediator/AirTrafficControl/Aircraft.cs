95fefacebc119991498077506a5aef2b2d8ef6e5
bondarV <147072539+bondarV@users.noreply.github.com>
2025-04-11 17:17:58 +0300

    Update Aircraft.cs

--- csharp/src/patterns/Mediator/AirTrafficControl/Aircraft.cs
+++ csharp/src/patterns/Mediator/AirTrafficControl/Aircraft.cs
@@ -3,35 +3,27 @@ namespace DesignPatterns.Mediator
   class  Aircraft
   {
     public string Name;
-    public Runway? CurrentRunway { get; set; }
+    public int Size { get; set; }
+    
+    public CommandCentre commandCentre;
+    
     public bool IsTakingOff { get; set; }
-    public Aircraft(string name, int size)
+    public Runway? CurrentRunway { get; set; }
+
+    public Aircraft(string name, int size,CommandCentre commandCentre)
     {
       this.Name = name;
+      this.Size = size;
+      this.commandCentre = commandCentre;
     }
+    
     public void Land(Runway runway)
     {
-      Console.WriteLine($"Aircraft {this.Name} is landing.");
-      Console.WriteLine($"Checking runway.");
-      if (runway.IsBusyWithAircraft == null)
-      {
-        Console.WriteLine($"Aircraft {this.Name} has landed.");
-        runway.IsBusyWithAircraft = this;
-        runway.HighLightRed();
-        this.CurrentRunway = runway;
-      }
-      else
-      {
-        Console.WriteLine($"Could not land, the runway is busy.");
-      }
+      commandCentre.LandPlane(runway,this);
     }
     public void TakeOff(Runway runway)
     {
-      Console.WriteLine($"Aircraft {this.Name} is taking off.");
-      runway.IsBusyWithAircraft = null;
-      this.CurrentRunway = null;
-      runway.HighLightGreen();
-      Console.WriteLine($"Aircraft {this.Name} has took off.");
+      commandCentre.TakeOff(runway,this);
     }
   }
 }
